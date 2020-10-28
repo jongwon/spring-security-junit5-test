285d11ece296a904ca372896365c460ca1529ba9
jongwon <jongwons.choi@gmail.com>
2020-10-28 16:16:08 +0900

    add

--- security-basic/src/main/java/com/sp/sec/basic/HomeController.java
+++ security-basic/src/main/java/com/sp/sec/basic/HomeController.java
@@ -3,10 +3,8 @@ package com.sp.sec.basic;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
