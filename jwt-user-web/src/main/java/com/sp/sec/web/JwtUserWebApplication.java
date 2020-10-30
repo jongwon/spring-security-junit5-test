085f98c10b0fcd0d71be7c73539697bfa107146c
jongwon <jongwons.choi@gmail.com>
2020-10-30 11:09:32 +0900

    add

---
+++ jwt-user-web/src/main/java/com/sp/sec/web/JwtUserWebApplication.java
@@ -0,0 +1,13 @@
+package com.sp.sec.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUserWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtUserWebApplication.class, args);
+	}
+
+}
