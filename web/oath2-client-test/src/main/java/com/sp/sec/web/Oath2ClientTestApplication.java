4ec46ce170e885b6de8cc943a11d7dcf510abb56
jongwon <jongwons.choi@gmail.com>
2020-11-18 10:15:39 +0900

    1

---
+++ web/oath2-client-test/src/main/java/com/sp/sec/web/Oath2ClientTestApplication.java
@@ -0,0 +1,13 @@
+package com.sp.sec.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2ClientTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oath2ClientTestApplication.class, args);
+	}
+
+}
